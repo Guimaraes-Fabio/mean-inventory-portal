131c01adc4bb17f428c745ad627f97707a0dd9c7
Guimaraes-Fabio <ffilho01@mynbcc.ca>
2026-06-03 20:27:48 -0300

    final changes

--- JWTClient/src/app/auth/services/authentication-service.ts
+++ JWTClient/src/app/auth/services/authentication-service.ts
@@ -11,7 +11,7 @@ import { LoggedIn } from '../../models/logged-in';
   providedIn: 'root',
 })
 export class AuthenticationService {
-  private readonly API_URL: string = 'https://inventorymanagementapi-6ha93cui.b4a.run';
+  private readonly API_URL: string = 'https://inventorymanagementapi-hb380pm7.b4a.run';
   private tokenTimer: any;
   private isAuthenticated: boolean = false;
   private username?: string;
