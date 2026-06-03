c957ab5012bec75abe080143630b2cd6b5c401f0
Guimaraes-Fabio <ffilho01@mynbcc.ca>
2026-06-03 20:12:14 -0300

    testing changes

--- JWTServer/src/db/mongoose.js
+++ JWTServer/src/db/mongoose.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB);
+mongoose.connect(process.env.MONGODB)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => {
+    console.error('MongoDB connection failed:', err.message);
+    process.exit(1);
+  });
