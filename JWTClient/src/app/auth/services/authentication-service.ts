93eb0139a2a194cfa30b39aa19a7a1f92ec41de2
Guimaraes-Fabio <ffilho01@mynbcc.ca>
2026-06-05 18:20:22 -0300

    changed the links for the API

--- JWTClient/src/app/auth/services/authentication-service.ts
+++ JWTClient/src/app/auth/services/authentication-service.ts
@@ -11,7 +11,7 @@ import { LoggedIn } from '../../models/logged-in';
   providedIn: 'root',
 })
 export class AuthenticationService {
-  private readonly API_URL: string = 'https://mean-inventory-portal-4i7b.vercel.app/';
+  private readonly API_URL: string = 'https://mean-inventory-portal-4i7b.vercel.app';
   private tokenTimer: any;
   private isAuthenticated: boolean = false;
   private username?: string;
